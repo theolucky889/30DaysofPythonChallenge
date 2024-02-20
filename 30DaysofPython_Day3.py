8e968f3e95a8960db0c3f022dc5717de97076920
theolucky889 <111281171+theolucky889@users.noreply.github.com>
2024-02-20 23:47:35 +0800

    Add files via upload

---
+++ 30DaysofPython_Day3.py
@@ -0,0 +1,45 @@
+#Boolean
+"""A boolean data type represents one of the two values: True or False. The use of these data
+types will be clear once we start using the comparison operator. The first letter T for True
+and F for False should be capital unlike JavaScript.
+    """
+    #Example
+print(True)
+print(False)
+
+#Assignment Operators
+"""Assignment operators are used to assign values to variables. Let us take = as an example.
+Equal sign in mathematics shows that two values are equal, however in Python it means we are
+storing a value in a certain variable and we call it assignment or a assigning value to a 
+variable. 
+    """
+#Arithmetic Operators:
+"""
+Addition(+): a + b
+Subtraction(-): a - b
+Multiplication(*): a * b
+Division(/): a / b
+Modulus(%): a % b
+Floor division(//): a // b
+Exponentiation(**): a ** b
+    """
+    
+#Example: Integers
+print('Addition: ', 1 + 2)
+print('Subtraction: ', 2 - 1)
+print('Multiplication: ', 2 * 3)
+print('Division: ', 4 / 2)          #Division in Python gives floating number
+print('Divison: ', 7 / 2)
+print('Division without remainder: ', 7 // 2)   #Goves result without the floating number or without remainder
+print('Division without remainder: ', 7 // 3)
+print('Modulus: ', 3 % 2)           #Modulus = Gives the remainder
+print('Exponentiation: ', 2 ** 3)   #Pangkat = 2 pangkat 3 (2 ** 3)
+
+#Example: Floats
+print('Floating Point Number, PI', 3.14)
+print('Floating Point Number, gravity', 9.81)
+
+#Example: Complex Number
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ', (1 + 1j) * (1 - 1j))
+
