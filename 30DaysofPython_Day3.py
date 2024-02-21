de27cade44226838ff9f9d1f1fa66cbb20060a47
theolucky889 <111281171+theolucky889@users.noreply.github.com>
2024-02-22 00:11:11 +0800

    Update 30DaysofPython_Day3.py

--- 30DaysofPython_Day3.py
+++ 30DaysofPython_Day3.py
@@ -108,5 +108,62 @@ print(weight, 'N')  # Adding unit to the weight
 # Calculate the density of a liquid
 mass = 75   # in KG
 volume = 0.075  # in cubic meter
-density = mass / volume     # 1000 Kg/m^34
+density = mass / volume     # 1000 Kg/m^3
 print(density, 'Kg/m^3')
+
+# Comparison Operators
+print(3 > 2)    # True, because 3 is greater than 2
+print(3 >= 2)   # True, because 3 is greater than 2\
+print(3 < 2)    # False, because 3 is greater than 2
+print(2 < 3)    # True, because 2 is less than 3
+print(2 <= 3)   # True, because 2 is less than 3
+print(3 == 2)   # False, because 3 is not equal to 2
+print(3 != 2)   # True, because 3 is not equal to 2
+print(len('mango') == len('avocado'))   # False
+print(len('mango') != len('avocado'))   # True
+print(len('mango') <  len('avocado'))   # True
+print(len('milk') != len('meat'))       # False
+print(len('milk') == len('meat'))       # True
+print(len('tomato') == len('potato'))   # True
+print(len('python') > len('dragon'))    # False
+
+# Comparing something gives either a True or False
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False: ', False == False)
+
+# In addition to the above comparison operator Python uses:
+"""
+- is: Returns true if both variables are the same object(x is y)
+- is not: Returns true if both variables are not hte same object(x is not y)
+- in: Returns True if the queried list contains a certain item(x in y)
+- not in: Returns True if the queried list doesn't have a certain item(x not in y)
+    """
+print('1 is 1', 1 is 1)                     # True, becasue the data values are the same
+print('1 is not 2', 1 is not 2)             # True, because 1 is not 2
+print('T in Theodore', 'T' in 'Theodore')   # True, T is found in the string
+print('E in Theodore', 'E' in 'Theodore')   # False, upper case E is not in the string
+print('coding' in 'coding for all')         # True, because coding for all has the word coding
+print('a in an:', 'a' in 'an')              # True
+print('4 is 2 ** 2', 4 is 2 ** 2)           # True
+
+# Logical Operators 
+"""
+- and = Returns True if both statements are True
+- or = Returns True if one of the statements is True
+- not = Reverse the result, returns False if the result is True
+    """
+
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False:', True or False)
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
