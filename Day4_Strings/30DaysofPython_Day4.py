f490e21146132325209d33f4e449d2eb9171be9f
theolucky889 <theolucky889@gmail.com>
2024-03-01 00:50:50 +0800

    Update 30DaysofPython_Day4.py

--- Day4_Strings/30DaysofPython_Day4.py
+++ Day4_Strings/30DaysofPython_Day4.py
@@ -39,4 +39,13 @@ print(len(full_name))
 - \" : double quote (")    
     """
 # Let's see the use of the above escape sequences with examples
+print("I hope I will be better at python. \nAm I ?")    # line break
+print("Days\tTopics\tExercises")    # adding tab spaces or 4 spaces
+print("Day 1\t5\t5")
+print("Day 2\t6\t20")
+print("Day 3\t5\t23")
+print("Day 4\t1\t35")
+print("This is a blackslash symbol (\\)")   # To write backslash
+print("In every programming language it starts with \"Hello, World!\"") # To write a double quote inside
+
 
