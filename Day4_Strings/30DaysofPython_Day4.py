de0fd09ee3f43c8142bdb31dc55ece2a8a1dcb49
theolucky889 <theolucky889@gmail.com>
2024-03-01 23:56:51 +0800

    Update 30DaysofPython_Day4.py

--- Day4_Strings/30DaysofPython_Day4.py
+++ Day4_Strings/30DaysofPython_Day4.py
@@ -63,3 +63,22 @@ like "%s", "%d", "%f", "%.number of digitsf".
     """
 
 # Strings only
+first_name = "Theodore Lucky"
+last_name = "Tendy"
+language = "Python"
+formated_string = "I am %s %s. I am learning %s" %(first_name, last_name, language)
+print(formated_string)
+
+# Strings and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = "The area of circle with a radius %d is %.2f" %(radius, area)     # 2 refers the 2 significant digits after the decimal point
+
+python_libraries = ['Django', 'Flask', 'NumPy', 'Matplotlib', 'Pandas']
+formated_string = "The following are python libraries:%s" %(python_libraries)
+print(formated_string)
+
+# NEW style string formatting (str.format)
+# Introduced in Python3
+
