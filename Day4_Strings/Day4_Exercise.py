50a00698305ce4171959913ec7eeea25a524e51e
theolucky889 <theolucky889@gmail.com>
2024-03-12 01:40:06 +0800

    Update Day4_Exercise.py

--- Day4_Strings/Day4_Exercise.py
+++ Day4_Strings/Day4_Exercise.py
@@ -1,1 +1,19 @@
 # 1. Concantate the string "Thirty", "Days", "Of", "Python" to a single string, "Thirty Days Of Python"
+days_python = ["Thirty", "Days", "Of", "Python"]
+result = " ".join(days_python)
+print(result)
+
+# 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+coding_all = ['Coding', 'For' , 'All']
+result = " ".join(coding_all)
+print(result)
+
+# 3. Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding For All"
+first_letter = company[0]
+
+# 4. Print the variable company using print()
+print(first_letter)
+
+# 5. Print the length of the company string using len() method and print()
+print(len(company))
