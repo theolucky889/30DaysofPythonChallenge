9066936bd45a7432425c4f1346a541750f655999
theolucky889 <theolucky889@gmail.com>
2024-03-20 19:41:31 +0800

    Update 30Days0fPython_Day5.py

--- Day5_Lists/30Days0fPython_Day5.py
+++ Day5_Lists/30Days0fPython_Day5.py
@@ -282,3 +282,21 @@ list1 = ["item1", "item2"]
 list2 = ["item3", "item4", "item5"]
 list1.extend(list2)
 print(list1)
+
+num1 = [0, 1, 2, 3]
+num2 = [4, 5, 6]
+num1.extend(num2)
+print("Numbers: ", num1)
+
+negative_numbers = [-5. -4, -3, -2, -1]
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+print("Integers: ", negative_numbers)
+
+fruits = ["banana", "orange", "mango", "lemon"]
+vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"]
+fruits.extend(vegetables)
+print("Fruits and Vegetables: ", fruits)
