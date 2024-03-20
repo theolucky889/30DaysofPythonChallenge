26c34efdde93786bc3f51555c60b599c6345e427
theolucky889 <theolucky889@gmail.com>
2024-03-21 00:08:53 +0800

    Update Exercise_Day5.py

---
+++ Day5_Lists/Exercise_Day5.py
@@ -0,0 +1,12 @@
+# 1. Declare an empty list
+empty_list = []
+print(empty_list)
+
+# 2. Declare a list with more than 5 items
+list_five = ["apple", "strawberry", "melon", "lime", "mango"]
+
+# 3. Find the length of your list
+print(len(list_five))
+
+# 4. Get the first item, the middle item and the last item of the list
+first_middle_last = list_five
