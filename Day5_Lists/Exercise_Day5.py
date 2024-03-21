4879f290083c87444496949b5187c026fc5b2759
theolucky889 <theolucky889@gmail.com>
2024-03-22 00:07:17 +0800

    Update Exercise_Day5.py

--- Day5_Lists/Exercise_Day5.py
+++ Day5_Lists/Exercise_Day5.py
@@ -42,3 +42,34 @@ print(it_companies)
 # 12. Insert an IT company in the middle of the companies list
 it_companies.insert(4, "BenQ")
 print(it_companies)
+
+# 13. Change one of the it_companies name to uppercase (IBM excluded)
+it_companies[1] = it_companies[1].upper()
+print(it_companies)
+
+# 14. Join the it_companies with a string "#"
+print("#".join(it_companies))
+
+# 15. Check if a certain company exists in the it_companies list
+def check_company(company, it_companies):
+    if company in it_companies:
+        return True, it_companies.index(company)
+    else:
+        return False, None
+
+user_input = input("Name a Company: ")
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+# Stores the result
+result = check_company(user_input, it_companies)
+# Output
+print(result)
+
+# 16. Sort the list using sort() method
+it_companies.sort()
+print(it_companies)
+
+# 17. Reverse the list in descending order using reverse() method
+it_companies.reverse()
+print(it_companies)
+
+# 18. Slice out the first 3 companies from the list
