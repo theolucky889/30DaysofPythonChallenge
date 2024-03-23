1d5b6639a08fcfa370190347d697ba01e29cd51c
theolucky889 <theolucky889@gmail.com>
2024-03-24 00:27:03 +0800

    Update 30DaysofPython_Day6.py

---
+++ Day6_Tuples/30DaysofPython_Day6.py
@@ -0,0 +1,11 @@
+# TUPLES
+"""
+A tuple is a collection of different data types which is ordered and unchangeable(immutable).
+Tuples are written with round brackets (). Once a tuple is created, we cannot change its values.
+We cannot use add, insert, remove methods in a tuple because it is not modifiable(mutable).
+Unlike list, tuple has few methods.
+- tuple(): to create an empty tuple
+- count(): to count the number of a specified item in a tuple
+- index(): to find the index of a specified item in a tuple
+- operator(): to join two or more tuples and create a new tuple
+"""
