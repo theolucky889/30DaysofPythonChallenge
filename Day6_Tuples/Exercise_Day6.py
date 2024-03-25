b61cc16ab2e4c802ca1c156a49325a05c700e669
theolucky889 <theolucky889@gmail.com>
2024-03-25 23:31:41 +0800

    Update Exercise_Day6.py

--- Day6_Tuples/Exercise_Day6.py
+++ Day6_Tuples/Exercise_Day6.py
@@ -26,3 +26,18 @@ print(siblings)
 family_members = tuple(siblings)
 print(family_members)
 
+# Exercise 2
+
+# 1. Unpack siblings and parents from family_members
+brother1, brother2, brother3, brother4, sister1, sister2, sister3, parent1, parent2 = family_members
+
+# 2. Create fruits, vegetables and animal products tuples.
+# Join the three tuples and assign it to a variable called food_stuff_tp
+fruits = ("apple", "banana", "orange", "mango")
+vegetables = ("spinach", "brocolli", "cucumber", "okra")
+animal_products = ("leash", "cage", "dog_food", "cat_food")
+
+food_stuff_tp = fruits + vegetables + animal_products
+
+# 2. Change the about food_stuff_tp tuple to a food_stuff_lt list
+food_stuff_lt = list(food_stuff_tp)
