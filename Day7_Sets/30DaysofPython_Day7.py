28e4980f5ca15e9a54bb17c1823a2e7f1475357c
theolucky889 <theolucky889@gmail.com>
2024-03-27 00:27:00 +0800

    Update 30DaysofPython_Day7.py

--- Day7_Sets/30DaysofPython_Day7.py
+++ Day7_Sets/30DaysofPython_Day7.py
@@ -67,3 +67,80 @@ print(fruits)
 the item is not found, remove() method will raise errors, so
 it is good to check if the item exist in the given set.
 However, discard() method doesn't raise any errors."""
+
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+st.remove('item2')
+
+# the pop() method removes a random item from a list and it returns the removed item
+# Example
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.pop()    # removes a random item from the set
+# If we are interested in the removed item
+removed_item = fruits.pop()
+
+# Clearing Items in a set
+# If we want to clear or empty the set, we use clear() method
+
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+st.clear()
+
+# Example
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.clear()
+print(fruits)   # set()
+
+# Deleting a set
+# If we want to delete the set itself, we can use the del operator
+
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+del st
+
+# example
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+del fruits
+# print(fruits)
+
+# Converting a list to a set
+# We can convert list to set and set to list. COnverting list to set removes duplicates and only unique items will be reserved
+
+# syntax
+lst = ['item1', 'item2', 'item3', 'item4', 'item1']
+st = set(lst)   # {'item2', 'item4', 'item1', 'item3'} - Order is random, because sets in general are unordered
+print(st)
+
+# Example: 
+fruits = ['banana', 'orange', 'mango', 'lemon', 'orange', 'banana']
+fruits = set(fruits)    # {'mango', 'lemon', 'banana', 'orange'}
+print(fruits)
+
+# Joining sets
+# We can join two sets using the union() or update() method
+
+# - Union(), this method returns a new set
+
+# syntax 
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item5', 'item6', 'item7', 'item8'}
+st3 = st1.union(st2)
+
+# Example
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+vegetables = {'tomato', 'potato', 'cabbage', 'onion', 'carrot'}
+fruits_vegetables = fruits.union(vegetables)
+print(fruits_vegetables)
+
+# - update(), This method inserts a set into a given set
+# syntax 
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item5', 'item6', 'item7', 'item8'}
+st1.update(st2)     # st2 contents are added to st1
+
+# Example
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+vegetables = {'tomato', 'potato', 'cabbage', 'onion', 'carrot'}
+fruits.update(vegetables)
+print(fruits)
+
