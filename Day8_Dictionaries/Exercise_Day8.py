27da2553c4c39c757c9ce502db66513285a2e568
theolucky889 <theolucky889@gmail.com>
2024-03-30 23:33:32 +0800

    Update Exercise_Day8.py

---
+++ Day8_Dictionaries/Exercise_Day8.py
@@ -0,0 +1,16 @@
+# 1. Create an empty dictionary called dog
+dog = {}
+
+# 2. Add name, color, breed, legs, age to the dog dictionary
+dog = {
+        'name':'Edison', 
+        'color':'white', 
+        'breed':'Pug', 
+        'legs':'four', 
+        'age':22
+}
+
+# 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city, and address as keys for the dictionary
+student = {
+    
+}
