756dde70ce00a8cc96789ce7147cb59a18c64538
theolucky889 <theolucky889@gmail.com>
2024-03-30 23:10:52 +0800

    Update 30DaysofPython_Day8.py

--- Day8_Dictionaries/30DaysofPython_Day8.py
+++ Day8_Dictionaries/30DaysofPython_Day8.py
@@ -100,3 +100,105 @@ print(person.get('city'))   # None
 
 # Adding Items to a Dictionary
 # We can add new key and value pairs to a dictionary
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct['key5'] = 'value5'
+
+# Example:
+person = {
+    'first_name':'Theodore Lucky',
+    'last_name':'Tendy',
+    'age':22,
+    'country':'Indonesia',
+    'is_married':False,
+    'skills':['Python', 'JS', 'HTML/CSS', 'React'],
+    'address':{
+        'street':'Taiyuan Street',
+        'zipcode':103,
+    }
+}
+person['job_title'] = 'Intern'
+person['skills'].append('Selenium')
+print(person)
+
+# Modifying items in a dictionary
+# We can modify items in a dictionary
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct['key1'] = 'value-one'
+
+# Example:
+person = {
+    'first_name':'Theodore Lucky',
+    'last_name':'Tendy',
+    'age':22,
+    'country':'Indonesia',
+    'is_married':False,
+    'skills':['Python', 'JS', 'HTML/CSS', 'React'],
+    'address':{
+        'street':'Taiyuan Street',
+        'zipcode':103
+    }
+}
+person['first_name'] = 'boy'
+person['age'] = 223
+print(person)
+
+# Checking Keys in a Dictionary
+# We use the in operator to check if a key exist in a dictionary
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print('key2' in dct)    # True
+print('key5' in dct)    # False
+
+# Removing Key and Value Pairs from a Dictionary
+"""
+- pop(key): removes the item with the specified key name:
+- popitem(): removes the last item
+- del: removes an item with specified key name
+"""
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct.pop('key1')     # remove key1 item
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct.popitem()   # removes the last item
+del dct['key2']
+
+# Example:
+person = {
+    'first_name':'Theodore Lucky',
+    'last_name':'Tendy',
+    'age':22,
+    'country':'Indonesia',
+    'is_married':False,
+    'skills':['Python', 'JS', 'HTML/CSS', 'React'],
+    'address':{
+        'street':'Taiyuan Street',
+        'zipcode':103
+    }
+}
+person.pop('first_name')
+person.popitem()
+del person['country']
+
+# Changing Dictionary to a list of items
+# the items() method changes dictionary to a list of tuples
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print(dct.items())
+# dct_items([('key1', 'value1'), ('key2', 'value2'), ('key3', 'value3'), ('key4', 'value4')])
+
+# Clearing a dictionary
+# if we don't want the items in a dictionary we can clear them using clear() method
+
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print(dct.clear())  # None
+
+# Deleting a Dictionary
+# if we do not use the dictionary we can delete it completely
