2d49227d5c14885520eb624a5333a1c2d6ce1c34
theolucky889 <theolucky889@gmail.com>
2024-04-01 13:39:14 +0800

    Update Exercise_Day8.py

--- Day8_Dictionaries/Exercise_Day8.py
+++ Day8_Dictionaries/Exercise_Day8.py
@@ -12,5 +12,44 @@ dog = {
 
 # 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city, and address as keys for the dictionary
 student = {
-    
+    'first_name':'Theodore Lucky',
+    'last_name':'Tendy',
+    'gender':'male',
+    'age':22,
+    'marital_status':False,
+    'skills':['Python', 'HTML', 'CSS', 'JS'],
+    'country':'Taiwan',
+    'city':'Taipei',
+    'address':{
+            'street':'Taiyuan Street',
+            'zipcode':103
+    }
 }
+
+# 4. Get the length of the student dictionary
+print(len(student))
+
+# 5. Get the value of skills and chekc the data type (should be a list)
+print(type(student['skills']))
+
+# 6. Modify the skills by adding one or two skills
+student['skills'].append('React')
+print(student)
+
+# 7. Get the dictionary keys as a list
+keys = student.keys()
+print(keys)
+
+# 8. Get the dictionary values as a list
+values = student.values()
+print(values)
+
+# 9. Change the dictionary to a list of tuples using items() method
+print(student.items())
+
+# 10. Delete one of the items in the dictionary
+del student['marital_status']
+print(student)
+
+# 11. Delete one of the dictionaries
+del student
