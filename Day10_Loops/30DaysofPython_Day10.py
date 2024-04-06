441aa13ab7402eab535df4657b8ab485e0640c1b
theolucky889 <theolucky889@gmail.com>
2024-04-07 01:10:28 +0800

    Update 30DaysofPython_Day10.py

--- Day10_Loops/30DaysofPython_Day10.py
+++ Day10_Loops/30DaysofPython_Day10.py
@@ -136,3 +136,156 @@ for number in numbers:
     print(number)
 
 # - for loop with dictionary looping through a dictionary gives you the key of the dictionary
+
+# syntax
+"""
+for iterator in dct:
+    code goes here
+"""
+
+# Example
+person = {
+    'first_name':'Theodore Lucky',
+    'last_name':'Tendy',
+    'age':22,
+    'country':'Taiwan',
+    'is_married':False,
+    'skills':['Python', 'Selenium', 'JavaScript', 'React'],
+    'address':{
+        'street':'TaiYuan Street',
+        'zipcode':103
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items():
+    print(key, value)
+
+# - Loops in set
+
+# syntax
+"""
+for iterator in st:
+    code goes here
+"""
+
+# Example
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'Viewsonic', 'Amazon'}
+for company in it_companies:
+    print(company)
+
+# Break and Continue Part2
+# short reminder: Break: WE use break when we like to stop our loop before it is completed
+
+# syntax
+"""
+for iterator in sequence:
+    code goes here
+    if condition:
+        break
+"""
+
+# Example:
+numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        break
+# The loop stops when it reaches 3
+
+# Continue: We use continue when we like to skip some of the steps in the iteration of the loop
+
+# syntax
+"""
+for iterator in sequence:
+    code goes here
+    if condition:
+        continue
+"""
+# Example:
+numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print('Next number should be ', number + 1) if number != 5 else print("loop's end") # for short hand conditions need both if and else statements
+    print('outside the loop')
+    # If the number equals 3, the step after the condition(but inside the loop) is skipped and the execution of the loop continues if there are any iterations left
+    
+# The range function
+"""
+The range() function is used for a list of numbers. THe range(start, end, step) takes three
+parameters: starting, ending and increment. By default it starts from 0 and the increment is 1
+The range sequence needs at least 1 argument(end).
+Creating sequences using range
+"""
+lst = list(range(11))
+print(lst) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+st = set(range(1, 11))  # 2 arguments indicate start and end of the sequence, step set to default 1
+print(st)   # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+lst = list(range(0, 11, 2))
+print(lst)  # [0, 2, 4, 6, 8, 10]
+st = set(range(0, 11, 2))
+print(st)   # {0, 2, 4, 6, 8, 10}
+
+# syntax
+""" for iterator in range(start, end, step): """
+
+# Example
+for number in range(11):
+    print(number)   # prints 0 to 10, not including 11
+
+# Nested for loop
+# we can write loops inside a loop
+
+# syntax
+"""
+for x in y:
+    for t in x:
+        print(t)
+"""
+
+# Example:
+person = {
+    'first_name':'Theodore Lucky',
+    'last_name':'Tendy',
+    'age':22,
+    'country':'Taiwan',
+    'is_married':False,
+    'skills':['Python', 'Selenium', 'JavaScript', 'React'],
+    'address':{
+        'street':'TaiYuan Street',
+        'zipcode':103
+    }
+}
+for key in person:
+    if key == 'skills':
+        for skill in person['skills']:
+            print(skill)
+
+# For Else
+# IF we want to execute some message when the loop ends, we use else.
+
+# syntax
+"""
+for iterator in range(start, end, step):
+    do something
+else:
+    print('The loop ended')
+"""
+# Example:
+for number in range(11):
+    print(number)
+else:
+    print('The loop stops at', number)
+    
+# Pass
+"""
+In python, when statement is required(after semicolon), but we don't like to execute any code,
+we can write the word pass to avoid erros. Also we can use it as a placeholder, for future statements
+"""
+# Example:
+for number in range(6):
+    pass
