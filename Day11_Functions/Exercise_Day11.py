b9f42d8552ee680e41a9288aacac2cbb1a3ded31
theolucky889 <theolucky889@gmail.com>
2024-04-09 23:59:39 +0800

    Update Exercise_Day11.py

---
+++ Day11_Functions/Exercise_Day11.py
@@ -0,0 +1,11 @@
+def sum_two_numbers(num1, num2):
+    total = num1 + num2
+    print(total)
+sum_two_numbers(3, 4)
+
+def area_of_circle(r):
+    PI = 3.14
+    area = PI * r * r
+    print(area)
+area_of_circle(9)
+
