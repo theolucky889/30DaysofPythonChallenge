88eed5bd6fd5058b4d8a1e6cc0bd7b699da8e946
theolucky889 <theolucky889@gmail.com>
2024-04-09 20:16:40 +0800

    Update 30DaysofPython_Day11.py

--- Day11_Functions/30DaysofPython_Day11.py
+++ Day11_Functions/30DaysofPython_Day11.py
@@ -102,3 +102,55 @@ def function_name(para1, para2):
 print(function_name(arg1, arg2))
 """
 # Example:
+def generate_full_name(first_name, last_name):
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+print('Full Name: ', generate_full_name('Theodore Lucky', 'Tendy'))
+
+def sum_two_numbers(num_one, num_two):
+    sum = num_one + num_two
+    return sum
+print('Sum of two numbers: ', sum_two_numbers(4, 9))
+
+def calculate_age(current_year, birth_year):
+    age = current_year - birth_year
+    return age
+print('Your age is: ', calculate_age(2024, 2002))
+
+def weight_of_object(mass, gravity):
+    weight = str(mass * gravity) + ' N'  # The value has to be changed to a string first
+    return weight
+print('Weight of an object in Newtons: ', weight_of_object(100, 9.81))
+
+# Passing Arguments with Key and Value
+
+# syntax
+# Declaring a function
+"""
+def function_name(para1, para2):
+    codes
+    codes
+# Calling function
+print(function_name(para1 = 'John', para2 = 'Doe'))     # the order of arguments does not matter
+"""
+
+# Example
+def print_fullname(firstname, lastname):
+    space = ' '
+    full_name = firstname + space + lastname
+    return full_name
+print(print_fullname(firstname = 'Theodore Lucky', lastname = 'Tendy'))
+
+def add_two_numbers(num1, num2):
+    total = num1 + num2
+    return total
+print(add_two_numbers(num2 = 3, num1 = 5))
+
+# Function Returning a Value Part2
+"""
+If we do not return a value with a function, then our function is returning None by default.
+To return a value with a function, we use the keyword return followed by the variable we are returning.
+We can return any kind of data types from a function
+"""
+# - Returning a string: Example
