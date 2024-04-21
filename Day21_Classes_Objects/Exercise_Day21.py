58e904846af3a21166880ed0d7c20d52073b5950
theolucky889 <theolucky889@gmail.com>
2024-04-22 00:08:38 +0800

    Update Exercise_Day21.py

--- Day21_Classes_Objects/Exercise_Day21.py
+++ Day21_Classes_Objects/Exercise_Day21.py
@@ -54,3 +54,87 @@ print(p)
 Lucky
 <__main__.Person object at 0x00000295C755B1A0>
 '''
+
+# Let's add more parameters to the constructor function.
+class Person:
+    def __init__(self, firstname, lastname, age, country, city):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+        self.city = city
+        
+p = Person('Theodore Lucky', 'Tendy', 22, 'Taiwan', 'Taipei')
+print(p.firstname)
+print(p.lastname)
+print(p.age)
+print(p.country)
+print(p.city)
+
+# output
+'''
+Theodore Lucky
+Tendy
+22
+Taiwan
+Taipei
+'''
+
+# Object Methos
+# Objects can have methods. The methods are functions which belong to the object
+# Example:
+class Person:
+    def __init__(self, firstname, lastname, age, country, city):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country 
+        self.city = city
+    def person_info(self):
+        return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}'
+    
+p = Person('Theodore Lucky', 'Tendy', 22, 'Taiwan', 'Taipei')
+print(p.person_info())
+
+# output
+'''
+Theodore Lucky Tendy is 22 years old. He lives in Taipei, Taiwan
+'''
+
+# Object Default Methods
+'''
+Sometimes, you may want to have a default values for your object methods.
+If we give default values for the parameters in the constructor, we can avoid errors when we
+call or instantiate our class without parameters.
+'''
+class Person:
+    def __init__(self, firstname='Theodore Lucky', lastname='Tendy', age=22, country='Taiwan', city='Taipei'):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+        self.city = city
+    
+    def person_info(self):
+        return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}'
+    
+p1 = Person()
+print(p1.person_info())
+p2 = Person('Joesline', 'Suryadi', 25, 'Indonesia', 'Tangerang')
+print(p2.person_info())
+
+# output
+'''
+Theodore Lucky is 22 years old. He lives in Taipei, Taiwan
+Joesline Suryadi is 25 years old. He lives in Tangerang, Indonesia
+'''
+
+# Method to Modify Class Default Values
+'''
+In the example below, the person class, all the constructor parameters have default values.
+In addition to that, we have skills parameter, which we can access using a method.
+Let us create add_skill method to add skills to the skills list
+'''
+
+class Person:
+    
