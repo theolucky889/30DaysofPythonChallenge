36ff79ea9c99d61bea8f137e3a47df3a5d7bb1bc
theolucky889 <theolucky889@gmail.com>
2024-04-22 01:36:59 +0800

    Update Exercise_Day21.py

--- Day21_Classes_Objects/Exercise_Day21.py
+++ Day21_Classes_Objects/Exercise_Day21.py
@@ -137,4 +137,112 @@ Let us create add_skill method to add skills to the skills list
 '''
 
 class Person:
+    def __init__(self, firstname='Theodore Lucky', lastname='Tendy', age=22, country='Taiwan', city='Taipei'):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+        self.city = city
+        self.skills = []
+        
+    def person_info(self):
+        return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}'
+    def add_skills(self, skills):
+        self.skills.append(skills)
+
+p1 = Person()
+print(p1.person_info())
+p1.add_skills('Python')
+p1.add_skills('JavaScript')
+p1.add_skills('HTML')
+p2 = Person('Joesline', ' Suryadi', 22, 'Indonesia', 'Tangerang')
+print(p2.person_info())
+print(p1.skills)
+print(p2.skills)
+
+# output
+'''
+Theodore Lucky Tendy is 22 years old. He lives in Taipei, Taiwan
+Joesline  Suryadi is 22 years old. He lives in Tangerang, Indonesia
+['Python', 'JavaScript', 'HTML']
+[]
+'''        
+# Inheritance
+'''
+Using inheritance we can reuse parent class code. Inheritance allows us to define a class that
+inherits all the methods and properties from parent class. The parent class or super or base
+class is the class which gives all the methods and properties. Child class is the class that
+inherits from another or parent class. Let us create a student class by inheritng from person class
+'''        
+
+class Student(Person):
+    pass
+
+s1 = Student('Theodore', 'Lucky', 22, 'Taiwan', 'Taipei')
+s2 = Student('Joesline', 'Suryadi', 25, 'Indonesia', 'Tangerang')
+print(s1.person_info())
+s1.add_skills('JavaScript')
+s1.add_skills('React')
+s1.add_skills('Python')
+print(s1.skills)
+
+print(s2. person_info())
+s2.add_skills('Organizing')
+s2.add_skills('Marketing')
+s2.add_skills('Digital Marketing')
+print(s2.skills)
+
+# output
+'''
+Theodore Lucky is 22 years old. He lives in Taipei, Taiwan
+['JavaScript', 'React', 'Python']
+Joesline Suryadi is 25 years old. He lives in Tangerang, Indonesia
+['Organizing', 'Marketing', 'Digital Marketing']
+'''
+
+'''
+We did not call the init() constructor in the child class. If we didn't call it then we can still
+access all the properties from the parent. But if we do call the constructor we can access the
+parents properties by calling super.
+We can add a new methods to the child or we can override the parent class methods by creating
+the same method name in the child class. When we add the init() function, the child class will
+no longer inherit the parent's init() function.
+'''
+
+# Overriding parent method
+class Student(Person):
+    def __init__(self, firstname='Theodore Lucky', lastname='Tendy', age=22, country='Taiwan', city='Taipei', gender='Male'):
+        self.gender = gender
+        super().__init__(firstname, lastname, age, country, city)
+    def person_info(self):
+        gender = 'He' if self.gender =='Male' else 'She'
+        return f'{self.firstname} {self.lastname} is {self.age} years old. {gender} lives in {self.city}, {self.country}'
     
+s1 = Student('Theodore', 'Lucky', 22, 'Taiwan', 'Taipei', 'Male')
+s2 = Student('Joesline', 'Suryadi', 25, 'Indonesia', 'Tangerang', 'Female')
+print(s1.person_info())
+s1.add_skills('JavaScript')
+s1.add_skills('React')
+s1.add_skills('Python')
+print(s1.skills)
+
+print(s2. person_info())
+s2.add_skills('Organizing')
+s2.add_skills('Marketing')
+s2.add_skills('Digital Marketing')
+print(s2.skills)
+
+# output
+'''
+Theodore Lucky is 22 years old. He lives in Taipei, Taiwan
+['JavaScript', 'React', 'Python']
+Joesline Suryadi is 25 years old. She lives in Tangerang, Indonesia
+['Organizing', 'Marketing', 'Digital Marketing']
+'''
+
+'''
+We can use super() built-in function or the parent anme Person to automatically inherit
+the methods and properties from its parent. In the example above we can override the parent
+method. THe child methods has a differnet feature, it can identify, if the gender is male or female
+and assign the proper pronoun
+'''
