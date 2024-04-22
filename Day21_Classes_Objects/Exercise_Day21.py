76044edf54d4dd8eebec2c0aee051d1519e718ae
theolucky889 <theolucky889@gmail.com>
2024-04-23 00:57:30 +0800

    Update Exercise_Day21.py

---
+++ Day21_Classes_Objects/Exercise_Day21.py
@@ -0,0 +1,43 @@
+# Exercise Level 1
+'''
+1. Python has the module called statistics and we can use this module to do all the statistical
+calculations. However, to learn how to make function and reuse function, let us try to develop
+a program, which calculates the measure of central tendency of a sample(mean, median, mode) and
+measure of variability(range, variance, standard deviation). In addition to those measures, 
+find the min, max, count, percentile, and frequency distribution of the sample. You can create
+a class called Statistics and create all the functions that do statistical calculations as
+methods for the statistics class.
+Output:
+ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+print('Count:', data.count()) # 25
+print('Sum: ', data.sum()) # 744
+print('Min: ', data.min()) # 24
+print('Max: ', data.max()) # 38
+print('Range: ', data.range() # 14
+print('Mean: ', data.mean()) # 30
+print('Median: ', data.median()) # 29
+print('Mode: ', data.mode()) # {'mode': 26, 'count': 5}
+print('Standard Deviation: ', data.std()) # 4.2
+print('Variance: ', data.var()) # 17.5
+print('Frequency Distribution: ', data.freq_dist()) # [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+
+# your output should look like this
+print(data.describe())
+Count: 25
+Sum:  744
+Min:  24
+Max:  38
+Range:  14
+Mean:  30
+Median:  29
+Mode:  (26, 5)
+Variance:  17.5
+Standard Deviation:  4.2
+Frequency Distribution: [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+'''
+import statistics
+
+class Statistics:
+    def __init__(self, ):
+        
