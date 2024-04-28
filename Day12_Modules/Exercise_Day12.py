956c9f7a1185a950d2a6f7733cf0f34652465d79
theolucky889 <theolucky889@gmail.com>
2024-04-28 08:34:07 +0800

    Update Exercise_Day12.py

--- Day12_Modules/Exercise_Day12.py
+++ Day12_Modules/Exercise_Day12.py
@@ -53,3 +53,31 @@ and first 6 letters of the alphabet, a-f. Check the task 6 for output examples)
 def list_of_hexa_colors(n):
     hex_chars = '0123456789abcdef'
     return ['#' + ''.join(choices(hex_chars, k=6))for _ in range(n)]
+
+def list_of_rgb_colors(n):
+    return [f'rgb({randint(0, 255)},{randint(0, 255)},{randint(0, 255)})' for _ in range(n)]
+
+def generate_colors(color_type, n):
+    if color_type == 'hex':
+        return list_of_hexa_colors(n)
+    elif color_type == 'rgb':
+        return list_of_rgb_colors(n)
+    else:
+        return ['Invalid Input']
+
+print(generate_colors('hex', 3))  # Outputs something like ['#a3e12f', '#03ed55', '#eb3d2b']
+print(generate_colors('hex', 1))  # Outputs something like ['#b334ef']
+print(generate_colors('rgb', 3))   # Outputs something like ['rgb(5, 55, 175)', 'rgb(50, 105, 100)', 'rgb(15, 26, 80)']
+print(generate_colors('rgb', 1))   # Outputs something like ['rgb(33, 79, 176)']
+
+# Exercise Level 3
+'''
+1. Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+2. Write a function which returns an array of seven random numbers in range of 0-9. All the numbers should be unique
+'''
+from random import sample
+
+def shuffle_list():
+    return sample(range(10), 7)
+
+print(shuffle_list())
