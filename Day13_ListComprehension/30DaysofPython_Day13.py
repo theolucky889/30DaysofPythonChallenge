1d358ad190ba9a3b1c0396ddebed9542c19da2e3
theolucky889 <theolucky889@gmail.com>
2024-08-09 00:26:51 +0800

    Update 30DaysofPython_Day13.py

--- Day13_ListComprehension/30DaysofPython_Day13.py
+++ Day13_ListComprehension/30DaysofPython_Day13.py
@@ -48,3 +48,11 @@ odd_numbers = [i for i in range(21) if i % 2 != 0]      # to generate odd number
 print(odd_numbers)      # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 # filter numbers: filters out positive even numbers from the list below
+numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+positive_even_numbers = [i for i in numbers if i % 2 == 0 and i > 0]
+print(positive_even_numbers)    # [4, 6, 8, 10]
+
+# flattening a three dimensional array
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list =  [number for row in list_of_lists for number in row]
+print(flattened_list)   #[1, 2, 3, 4, 5, 6, 7, 8, 9]
