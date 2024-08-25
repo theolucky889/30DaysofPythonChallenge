abd4da974ea1268fbeaefb565bb329c78db2aaba
theolucky889 <theolucky889@gmail.com>
2024-08-25 21:46:06 +0800

    Update 30DaysofPython_Day14.py

--- Day14_HigherOrderFunctions/30DaysofPython_Day14.py
+++ Day14_HigherOrderFunctions/30DaysofPython_Day14.py
@@ -52,3 +52,20 @@ result = higher_order_function('absolute')
 print(result(-3))   # 3
 
 # as we can see from the above example that the higher order function is returning different functions depending on the passed parameter
+
+# Python Closures
+
+"""
+Python allows a nested function to access the outer scope of the enclosing function. This is known as a Closure.
+In Python, closure is created by nesting a function inside another encapsulating function and then returning the inner function
+"""
+
+# Example
+def add_ten():
+    ten = 10
+    def add(num):
+        return num + ten
+    return add
+closure_result = add_ten()
+print(closure_result(5))    # 15
+print(closure_result(10))   # 20
