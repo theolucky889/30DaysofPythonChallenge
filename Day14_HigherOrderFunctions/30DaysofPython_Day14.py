20f733d14761dd257c427d93bfcab74706a66828
theolucky889 <theolucky889@gmail.com>
2024-08-26 19:44:59 +0800

    Update 30DaysofPython_Day14.py

--- Day14_HigherOrderFunctions/30DaysofPython_Day14.py
+++ Day14_HigherOrderFunctions/30DaysofPython_Day14.py
@@ -122,3 +122,41 @@ def print_full_name(first_name, last_name, country):
     print("I am {} {}. I love to learn.".format(first_name, last_name, country))
 
 print_full_name("Theodore Lucky", "Tendy", "Taiwan")
+
+# Built-In Higher Order Functions
+'''Some of the built-in higher order functions that we cover in this part are map(), filter,
+and reduce.Lambda function can be passed as a parameter and the best use case of lambda 
+functions like map, filter and reduce'''
+
+# Python - Map Function
+'''The map() function is a built-in function that takes a function and iterable as paramters'''
+# syntax
+map(function, iterable)
+
+# Ex 1
+numbers = [1, 2, 3, 4, 5]   # iterable
+def square(x):
+    return x ** 2
+numbers_squared = map(square, numbers)
+print(list(numbers_squared))    # [1, 4, 9, 16, 25]
+# Lets apply it with a lambda function
+numbers_squared = map(lambda x : x ** 2, numbers)
+print(list(numbers_squared))    # [1, 4, 9, 16, 25]
+
+# Ex2
+numbers_str = ['1', '2', '3', '4', '5']     # Iterable
+numbers_int = map(int, numbers_str)
+print(list(numbers_int))    # [1, 2, 3, 4, 5]
+
+# ex 3
+names = ['Theodore', 'Lucky', 'Tendy', 'Valencia']  # iterable
+
+def change_to_upper(name):
+    return name.upper()
+
+names_upper_cased = map(change_to_upper, names)
+print(list(names_upper_cased))  # ['THEODORE', 'LUCKY', 'TENDY', 'VALENCIA']
+
+# lets use the lambda function
+names_upper_cased = map(lambda name: name.upper(), names)
+print(list(names_upper_cased))  # ['THEODORE', 'LUCKY', 'TENDY', 'VALENCIA']
